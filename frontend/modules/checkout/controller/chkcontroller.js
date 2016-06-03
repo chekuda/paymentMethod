b8228806536bd7a7aaa5d1c54d7c6e371b17616d
Chekuda <sochekilla@gmail.com>
2016-06-03 17:25:49 +0100

    frontend created checkout page client_id and client_secret added everything by ng-cli

---
+++ frontend/modules/checkout/controller/chkcontroller.js
@@ -0,0 +1,7 @@
+angular.module('paymentCtr',[]).
+controller('paymentMethod', function($scope,$http,$window) {
+
+
+	
+
+});
---
+++ frontend/public/js/main.js
@@ -0,0 +1,9 @@
+   /**
+      *Main Controller
+      @vepromptctr: vePrompt tool controller module
+      @insertImagestoDB: Insert images in DB (administrator)
+      @vecontactCtr: veContact tool controller module
+    **/
+
+    var app = angular.module('paymentMethod', ['paymentCtr']);
+
