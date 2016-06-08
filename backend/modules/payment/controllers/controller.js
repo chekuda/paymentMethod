b908a2412b9b36e006db60a0faff49baed7ee426
Chekuda <sochekilla@gmail.com>
2016-06-08 18:10:40 +0100

    function for call the API in order to create a payment

--- backend/modules/payment/controllers/controller.js
+++ backend/modules/payment/controllers/controller.js
@@ -1,13 +1,16 @@
 
 var express = require('express');
 var bodyParser = require("body-parser");
-var paypalM = require("../services/getUserId");
+var getUser = require("../services/getUserId");
+var acceptbypaypal = require("../services/paymentbypaypal");
 
 var listFunctions = express.Router();
 
 //In case I receive any JSON data
 listFunctions.use(bodyParser.json());
 
-listFunctions.get("/getUserId",paypalM.getUserId);//Login function
+listFunctions.get("/getUserId",getUser.getUserId);//Get crient_id and client_secret
+
+listFunctions.post("/connectPaypal",acceptbypaypal.acceptpaybypaypal);//geToken, create a payment
 
 exports.listFunctions = listFunctions;
