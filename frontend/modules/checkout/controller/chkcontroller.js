da0dd4aa877a24b2278bbfd63a9b92c7d276b4a4
Chekuda <sochekilla@gmail.com>
2016-06-09 12:16:37 +0100

    funtion for execute the payment added

--- frontend/modules/checkout/controller/chkcontroller.js
+++ frontend/modules/checkout/controller/chkcontroller.js
@@ -77,60 +77,77 @@ controller('paymentMethod', function($scope,$http,$window) {
 
 	$scope.checkifApprovedByPaypal = function(){
 
-		if(location.search)
-		{
+    if(location.search)
+    {
+      $scope.executePaymentDetails=
+      {
+        paymentByURL: location.search.match(/PAY.\w+/),
+        tokenByURL: sessionStorage.getItem("tokenPaypal"),//location.search.match(/EC.\w+/);
+        payerByURL: location.search.match(/\w+\w$/)
+      }
+      //If everything is right
+      if($scope.executePaymentDetails.tokenByURL)
+      {
+        if($scope.executePaymentDetails.paymentByURL && $scope.executePaymentDetails.payerByURL)
+         {
+          //    //@paymentId payerByURL[0]
 
-			$scope.paymentByURL = location.search.match(/PAY.\w+/);
-			$scope.tokenByURL = sessionStorage.getItem("tokenPaypal");//location.search.match(/EC.\w+/);
-			$scope.payerByURL = location.search.match(/\w+\w$/);
-			
-			if($scope.paymentByURL && $scope.tokenByURL && $scope.payerByURL){
-				//@paymentId payerByURL[0]
+             $http({
+              url: '/api/executepayment',
+              method: 'POST',
+              data: $scope.executePaymentDetails
+              })
+              .success(function (data){
+                console.log("PAYMENT SUCCESS>>>>>\n");
+                console.log(data);
+                sessionStorage.removeItem("tokenPaypal");//Removing the token
 
-					$http({
-			        url: 'https://api.sandbox.paypal.com/v1/payments/payment/'+$scope.paymentByURL[0]+'/execute/',
-			        method: 'POST',
-			        headers: {
-			        		 'Accept': 'application/json',
-			    			  'Content-Type': 'application/json',
-			                 'Authorization': 'Bearer ' + $scope.tokenByURL//Credentials for get the token
-			                  },
-			        data: JSON.stringify({payer_id: $scope.payerByURL[0]})
-			    	})
-					.success(function (data, status, headers, config){
-						console.log("PAYMENT SUCCESS>>>>>>>>>>"+data.transactions);
-						$scope.displayAlert = "success";
-						//Total->data.transactions[0].amount.total
-						//Currency ->data.transactions[0].amount.currency
-						//Description that I added before ->data.transactions[0].description
+              })
+              .error(function(err){
+                console.log("PAYMENT FAILED>>>>>\n");
+                  console.log(err);
+              })
 
-					})
-					.error(function(err){
-						console.log(err.name+" : DebugID"+err.debug_id);
-						$scope.displayAlert = "fail";
-					})
+          }
+          else
+          {
+            console.log(">>>>>>>>>>>>>>>Error: Couldn't get the paymentId from URL");
+          }
+      }
+       
+    }
+    else
+    {
+        console.log(">>>>>>>>>>>>>>>No Checkout Page");
+    }
 
-			}
-			else{
-				console.log(">>>>>>>>>>>>>>>Error: Couldn't get the paymentId from URL");
-			}
-		}
-		else{
-			console.log(">>>>>>>>>>>>>>>Checkout page not approved yet");
-		}
 	}();
 
   /*******************************
     Calling the backEnd for Get the token and create a payment
   ********************************/
-  $scope.createApayment = function()
+  $scope.createApayment = function(methodSelected)
   {
+    //Choose the payment by the client
+    if(methodSelected == "creditCard")
+    {
+      $scope.paymentM = $scope.paypalCreditCardSale;
+    }
+    else if(methodSelected == "paypal")
+    {
+      $scope.paymentM = $scope.paypalSale;
+    }
+    else
+    {
+      console.log("Error:>>>>>Method wrong spel it");
+    }
+
     if(!location.search)
     {
       $http({
           url: '/api/connectPaypal',
           method: 'POST',
-          data: $scope.paypalSale
+          data: $scope.paymentM
         })
           .success(function (data)
             {//Receive the payment created and now I have to redirect
@@ -150,116 +167,22 @@ controller('paymentMethod', function($scope,$http,$window) {
             }
             else if($scope.paymentCreated.payer.payment_method == "credit_card")
             {
-              console.log("Message>>>>>"+ $scope.paymentCreated);
+              console.log("Credit Card Payment Success>>>>>\n")
+              sessionStorage.removeItem("tokenPaypal");//Remove the item in order to not load the page with the token in it
+              console.log("PAYID>>>>>>>>>>\n"+$scope.paymentCreated.id);
+              window.location = window.location.href + "?" +$scope.paymentCreated.id;//Reload the page with the PAYID in the URL
             }
-            
+            else
+            {
+              console.log("ERROR>>>>>>Wrong payment method selected");
+            }
          })
           .error(function(err){
             console.log(err);
           })
     }
-  }();
-
-  $scope.paypalPayment = function(methodSelected){//passing the value to check which paymentMethod is selected
-
-	$http.get('/api/getUserId')
-      .success(function(res){
-      	console.log(res.msg);
-      	$scope.client_id=res.cliendSecret.client_id;//Add the res value to client_id
-      	$scope.client_secret=res.cliendSecret.client_secret;//Add the res value to client_secret
-      	if(res.cliendSecret.client_id && res.cliendSecret.client_secret)
-      	{
-      		$scope.getToken(methodSelected);//Call the get token function
-
-      	}
-      	else{
-      		console.log("Error:>>>>>User_ID credential couldn't be retrieved from Backend");
-      	}
-      	
-      })
-      .error(function(data){
-        console.log("Error: Impossible to get the clientID>>>>>> "+data);
-      });
-	
-	
-  };
-
-  $scope.getToken = function(methodSelected){
-	//After to get the information, making a call for get the token in order to bein authorize for the payment
-  	$http({
-        url: 'https://api.sandbox.paypal.com/v1/oauth2/token',
-        method: 'POST',
-        headers: {
-        		      'Accept': 'application/json',
-    			       'Content-Type': 'application/x-www-form-urlencoded',
-                 'Authorization': 'Basic ' + btoa($scope.client_id+":"+$scope.client_secret)//Credentials for get the token
-                  },
-        data: 'grant_type=client_credentials'
-		})
-  		.success(function (data, status, headers, config) {
-            console.log(data);
-            $scope.token = data.access_token;//give the token to the $scope variable
-            sessionStorage.setItem("tokenPaypal", $scope.token);//save the token into the sessionstorage
-
-      		$scope.createPayment(methodSelected);//Calling the payment method
-        })
-        .error(function (data, status, headers, config) {
-            console.log("Error: Can't get the token with the credentials >>>>>>"+data)
-		});
-  };
-
-  $scope.createPayment = function(methodSelected){//need to receive a var for data to send
-
-  	if(methodSelected == "creditCard")
-  	{
-  		$scope.paymentM = $scope.paypalCreditCardSale;
-  	}
-  	else if(methodSelected == "paypal")
-  	{
-  		$scope.paymentM = $scope.paypalSale;
-  	}
-  	else
-  	{
-  		console.log("Error:>>>>>Method wrong spel it");
-  	}
-  	//if I get the token try to make payment
-     //@@$scope.token, token to get access
-    $http({
-    url: 'https://api.sandbox.paypal.com/v1/payments/payment',
-    method: 'POST',
-    headers: {
-        		 'Accept': 'application/json',
-    			   'Content-Type': 'application/json',
-                 'Authorization': 'Bearer ' + $scope.token//Token taken for the server
-              },
-          data: JSON.stringify($scope.paymentM)//This got the values from the creditCard or payPal
-          })//Information sent as a string to the paypal API
-          .success(function (data, status, headers, config)
-      	  {
-      	  	console.log(data);
-      	  	if(data.payer.payment_method == "paypal")
-      	  	{
-      	  		if(data.links[1].method == "REDIRECT")//Only for paypal
-      		  	{
-      		  		window.location = data.links[1].href;
-      		  	}
-      		  	else
-      		  	{
-      		  		console.log("Error: Couldnt redirect to paypal page");
-      		  	}
-      	  	}
-      	  	else if(data.payer.payment_method == "credit_card")
-      	  	{
-      	  		console.log("Message>>>>>"+data);
-      	  	}
-      	  	
-      	 })
-	  .error(function(data){
-        console.log("Error: Cant get access with the token >>>>>> "+data);
-        console.log($scope.token);
-      });
   }
 
-	
+
 
 });
